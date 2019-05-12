8c828d65ac34fdb9fbfa7aab4f3f599d3524900c
sivakalivela <kalivelashiva@gmail.com>
2019-05-12 18:02:22 +0530

    fgfdgfd

--- src/main/java/com/crm/qa/pages/TasksPage.java
+++ src/main/java/com/crm/qa/pages/TasksPage.java
@@ -3,5 +3,7 @@ package com.crm.qa.pages;
 import com.crm.qa.base.TestBase;
 
 public class TasksPage extends TestBase{
-
+	public static void main(String[] args){
+		System.out.println("skdjfld");
+	}
 }
